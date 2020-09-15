989a119586b1a860a201722053b731b3b3c32a81
dimashipicyn <kanifol_catz@mail.ru>
2020-09-15 19:48:24 +0700

    add scaner and dump in main.c simpletrone mashine

--- simpletron/srcs/main.c
+++ simpletron/srcs/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define MEMORY 100
+
 void	instruction(void)
 {
 	printf("*** Симплетрон приветствует вас! ***\n\
@@ -10,8 +12,48 @@ void	instruction(void)
 *** прекращения ввода программы введите число -99999.     ***\n");
 }
 
+void	scaner(int memory[])
+{
+	int tmp  = 0;
+
+	for (int i = 0; i < MEMORY; i++) {
+		if (i < 10)
+			printf("0");
+		printf("%d? ", i);
+		scanf("%d", &tmp);
+		if (tmp == -99999)
+			break ;
+		memory[i] = tmp;
+	}
+	printf("*** Загрузка программы завершена ***\n\
+*** Начинаю выполнение программы ***\n");
+}
+
+void	dump(int memory[])
+{
+	printf("ПАМЯТЬ:\n\
+	0	1	2	3	4	5	6	7	8	9");
+	for (int i = 0; i < MEMORY; i++) {
+		if (i % 10 == 0)
+			printf("\n%2d  ", i);
+		printf("+");
+		if (memory[i] / 1000 == 0)
+			printf("0");
+		if (memory[i] / 100 == 0)
+			printf("0");
+		if (memory[i] / 10 == 0)
+			printf("0");
+		printf("%d   ", memory[i]);
+	}
+	printf("\n");
+}
+
 int		main(void)
 {
+	int memory[100] = {0};
+
 	instruction();
+	scaner(memory);
+	dump(memory);
 	return (0);
 }
